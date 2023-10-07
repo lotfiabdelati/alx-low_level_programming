883e07730dd07ac45cfdb851823133a3fd138b83
root <root@DESKTOP-TBEKVPN>
2023-10-07 01:09:21 +0100

    ajout is zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%i is positive", n);
-	else if (n ==0)
+	else if (n == 0)
 		printf("%i is zero", n);
 	else
 		printf("%i is negative", n);
