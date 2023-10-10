0ac47ded8eac4fc8247f45a17f1350acf3a948fe
root <root@DESKTOP-TBEKVPN>
2023-10-10 01:03:39 +0100

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * description: print all single digit numbers of base 10 starting from 0
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 10)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	printf("\n");
+	return (0);
+}
