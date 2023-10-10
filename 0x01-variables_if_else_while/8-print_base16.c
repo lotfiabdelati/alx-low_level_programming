47c73b47f9194db9d748b785b790f6d8d2245ed2
root <root@DESKTOP-TBEKVPN>
2023-10-10 01:48:35 +0100

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * description: print all numbers
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a  10 ; a++)
+	{
+	putchar(a + '0');
+	if (i != 9)
+	{
+		putchar(',');
+	}
+}
+	putchar('\n');
+	return (0);
+}
