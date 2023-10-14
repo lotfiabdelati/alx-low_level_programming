e693eebd688da78f591a0b6d58725471d39cfee2
lotfiabdelati <lotfiabdelati9@gmail.com>
2023-10-13 17:29:24 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * main - print founction is negative or positive
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+
+	return (0);
