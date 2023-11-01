f9a53b2f360095c0ab2e93a2a71143ff5dc4ba4b
lotfiabdelati <lotfiabdelati9@gmail.com>
2023-11-01 16:53:20 -0700

    correct is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,7 @@
  * Return: 1 for upper letter or 0 for any else
  */
 
-int _isupper(int x)
-
+int _isupper(int c)
 {
-
-	if (x >= 65 && x <= 90)
-	{
-	return (1);
-	}
-	return (0);
+	return (c >= 'A' && c <= 'Z');
 }
